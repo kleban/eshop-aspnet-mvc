3f204a2e688ee0e2f1aac025d366cd8f4601b266
Yurii Kleban <yurakleban@gmail.com>
2026-03-26 11:13:45 +0200

    fix

--- src/eShop.App/Controllers/CategoryController.cs
+++ src/eShop.App/Controllers/CategoryController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace eShopMVC.App.Controllers;
 
+[Authorize]
 public class CategoryController : BaseController
 {
     public CategoryController(IUnitOfWork unitOfWork): base(unitOfWork) { }
